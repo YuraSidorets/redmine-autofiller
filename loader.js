ced1857cabb52e057ef8ea8e6158d1e996a73c74
Yurii Sydorets <yurii.sydorets@medtronic.com>
2022-04-27 02:01:43 +0300

    Fixed loader

--- loader.js
+++ loader.js
@@ -6,7 +6,7 @@
   $("<style type='text/css'>"+css+"</style>").appendTo("head");
   $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js",function(){
   $.getScript("https://code.jquery.com/jquery-migrate-1.4.1.js",function(){    
-   $.getScript("/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js",function(){
+   $.getScript(gH+"/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js",function(){
     $.getScript(gH+"/redmine-autofiller/"+br+"/setup.js",function(){
      var d=new Date(),c="ukr";
      $.getScript("https://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForMonth&jsonp=holidaysLoaded"+
